11647d0536869909d2f6fceae81fbb40a2c02a69
phmcastro702 <phm.castro@hotmail.com>
2021-03-07 19:55:56 -0300

    Get de valores da UI
    Valores setados na UI são usados em tempo real para o lançamento da bola

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -5,30 +5,58 @@ using UnityEngine;
 public class BallController : MonoBehaviour
 {
     private Rigidbody ballRb;
-    public float throwForceFoward = 100;
-    public float throwForceUp = 100;
-    public float angle = 45;
+    public float throwForce = 100;
+    public float angle;
     private Vector3 throwDirection;
+
+    private Vector3 originalPos;
 
     void Start()
     {
         ballRb = transform.GetComponent<Rigidbody>();
 
+        angle = 45;
+
         throwDirection = new Vector3(0, Mathf.Sin(Mathf.Deg2Rad * angle), Mathf.Cos(Mathf.Deg2Rad * angle));
 
         Debug.DrawRay(transform.position, throwDirection, Color.blue, 20);
+
+        originalPos = transform.position;
 
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            //var dir = new Vector3(0, Mathf.Sin(Mathf.Deg2Rad * angle), Mathf.Cos(Mathf.Deg2Rad * angle));
-            Debug.Log("mouse pressed!");
-            ballRb.AddForce(throwDirection * throwForceFoward);
-            //ballRb.AddForce(throwDirection * throwForceUp);
-
+            ThrowBall();
+            Invoke("ResetBallPosition", 4);
         }
+    }
+
+    private void ThrowBall()
+    {
+        Debug.Log("mouse pressed!");
+        throwDirection = new Vector3(0, Mathf.Sin(Mathf.Deg2Rad * angle), Mathf.Cos(Mathf.Deg2Rad * angle));
+        ballRb.AddForce(throwDirection * throwForce);
+    }
+
+
+    public void ListenToAngleValue(float value)
+    {
+        angle = Mathf.RoundToInt(value);
+    }
+
+    public void ListenToForceValue(float value)
+    {
+        throwForce = Mathf.RoundToInt(value);
+    }
+
+
+    private void ResetBallPosition()
+    {
+        ballRb.velocity = Vector3.zero;
+        ballRb.angularVelocity = Vector3.zero;
+        transform.position = originalPos;
     }
 }
---
+++ Assets/Scripts/LiveSliderValueText.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class LiveSliderValueText : MonoBehaviour
+{
+
+    private TextMeshProUGUI valueText;
+
+    private void Start() 
+    {
+        valueText = transform.GetComponent<TextMeshProUGUI>();
+        Debug.Log(valueText.transform.name);
+
+    }
+
+    public void OnAngleValueChanged(float value)
+    {
+        value = Mathf.RoundToInt(value);
+        valueText.text = $"{value}°";
+
+    }
+
+    public void OnForceValueChanged(float value)
+    {
+        valueText.text = $"{value}f";
+    }
+}
