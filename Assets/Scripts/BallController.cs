706bc078da7e9207d11831247cdfb78d6e611190
phmcastro702 <phm.castro@hotmail.com>
2021-03-07 18:15:46 -0300

    first commit
    files from asset folder

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    private Rigidbody ballRb;
+    public float throwForceFoward = 100;
+    public float throwForceUp = 100;
+    public float angle = 45;
+    private Vector3 throwDirection;
+
+    void Start()
+    {
+        ballRb = transform.GetComponent<Rigidbody>();
+
+        throwDirection = new Vector3(0, Mathf.Sin(Mathf.Deg2Rad * angle), Mathf.Cos(Mathf.Deg2Rad * angle));
+
+        Debug.DrawRay(transform.position, throwDirection, Color.blue, 20);
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            //var dir = new Vector3(0, Mathf.Sin(Mathf.Deg2Rad * angle), Mathf.Cos(Mathf.Deg2Rad * angle));
+            Debug.Log("mouse pressed!");
+            ballRb.AddForce(throwDirection * throwForceFoward);
+            //ballRb.AddForce(throwDirection * throwForceUp);
+
+        }
+    }
+}
